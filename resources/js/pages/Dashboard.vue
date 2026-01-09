44f45e7291fa371f17ab619e55fa78f7fcb2fd0c
Rommel7may <ml0997130@gmail.com>
2026-01-09 09:10:53 +0800

    New

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -8,12 +8,12 @@ import {
     EmptyMedia,
     EmptyTitle,
 } from '@/components/ui/empty';
+import { Skeleton } from '@/components/ui/skeleton';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import { ArrowUpRightIcon, FolderCode } from 'lucide-vue-next';
-
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -26,9 +26,14 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div
-            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
-        >
+        <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <!-- <div class=" space-x-4">
+            <Skeleton class="h-12 w-12 rounded-full" />
+            <div class="space-y-2">
+                <Skeleton class="h-4 w-[250px]" />
+                <Skeleton class="h-4 w-[200px]" />
+            </div>
+        </div> -->
             <Empty>
                 <EmptyHeader>
                     <EmptyMedia variant="icon">
@@ -39,6 +44,8 @@ const breadcrumbs: BreadcrumbItem[] = [
                         You haven't created any projects yet. Get started by
                         creating your first project.
                     </EmptyDescription>
+                     <Skeleton class="h-4 w-[200px]" />
+                            <Skeleton class="h-4 w-[100px]" />
                 </EmptyHeader>
                 <EmptyContent>
                     <div class="flex gap-2">
